f550bae6a12bf1a6c2b395dfa78a1fadc097bd2a
dosfx <andrew.adbrown@gmail.com>
2025-12-12 15:14:58 -0500

    even better

--- 2025/7/part2.py
+++ 2025/7/part2.py
@@ -4,23 +4,15 @@ from aoc_lib import StrGrid
 
 grid = StrGrid.from_file("input.txt")
 
+
 @functools.cache
 def paths(x: int, y: int) -> int:
-    assert(grid.at(x, y) == "^")
-    left = 1
-    for yy in range(y + 1, grid.height):
-        if grid.at(x - 1, yy) == "^":
-            left = paths(x - 1, yy)
-            break
-    right = 1
-    for yy in range(y + 1, grid.height):
-        if grid.at(x + 1, yy) == "^":
-            right = paths(x + 1, yy)
-            break
-    return left + right
+    c = grid.at_none(x, y)
+    if c is None:
+        return 1
+    if c == "." or c == "S":
+        return paths(x, y + 1)
+    return paths(x - 1, y) + paths(x + 1, y)
 
-x, _ = next(grid.find("S"))
-for y in range(0, grid.height):
-    if grid.at(x, y) == "^":
-        print(paths(x, y))
-        break
+
+print(paths(*next(grid.find("S"))))
